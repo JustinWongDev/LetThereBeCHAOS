180fc0da0f15f7597372cb4552d29b6b600b7ac2
JustinWongDev <42408284+JustinWongDev@users.noreply.github.com>
2021-08-24 18:19:07 +1000

    no message

---
+++ CHAOS/Assets/Utility/FollowPlayerOnYAxis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerOnYAxis : MonoBehaviour
+{
+    private PlayerController player = null;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.y = player.transform.position.y;
+
+        transform.position = newPos;
+    }
+}
