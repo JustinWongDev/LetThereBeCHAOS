189a677a3ebb078b44d6ee314767ab468160d9d5
JustinWongDev <42408284+JustinWongDev@users.noreply.github.com>
2021-08-24 16:21:28 +1000

    Limit platforms to spawning within x-axis range

--- CHAOS/Assets/Platforms/PlatformGen.cs
+++ CHAOS/Assets/Platforms/PlatformGen.cs
@@ -7,6 +7,7 @@ public class PlatformGen : MonoBehaviour
     [Header("Var")]
     [SerializeField] private float yDistBetweenMax = 2.0f;
     [SerializeField] private float yDistBetweenMin = 1.0f;
+    [SerializeField] private float maxXDisplacement = 12.0f;
     [SerializeField] private float xDistBetweenMax = 10.0f;
     [SerializeField] private float xDistBetweenMin = 4.0f;
     [SerializeField] private int initNumPlatforms = 5;
@@ -40,7 +41,22 @@ public class PlatformGen : MonoBehaviour
     {
         for (int i = 0; i < numOfPlatforms; i++)
         {
-            spawnPt.x += Random.Range(Random.Range(-xDistBetweenMin, -xDistBetweenMax), Random.Range(xDistBetweenMin, xDistBetweenMax));
+            if (spawnPt.x >= 12)
+            {
+                spawnPt.x = 12;
+                spawnPt.x += Random.Range(Random.Range(-xDistBetweenMin, -xDistBetweenMax), 0);
+            }
+            else if (spawnPt.x <= -12)
+            {
+                spawnPt.x = -12;
+                spawnPt.x += Random.Range(0, Random.Range(xDistBetweenMin, xDistBetweenMax));
+            }
+
+            else
+            {
+                spawnPt.x += Random.Range(Random.Range(-xDistBetweenMin, -xDistBetweenMax), Random.Range(xDistBetweenMin, xDistBetweenMax));
+            }
+
             spawnPt.y += Random.Range(yDistBetweenMin, yDistBetweenMax);
 
             GameObject go = Instantiate(prefPlatform);
