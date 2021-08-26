3a6088174eb3af394b6180e0930dd9f80d60c0b8
JustinWongDev <42408284+JustinWongDev@users.noreply.github.com>
2021-08-26 19:59:22 +1000

    Added chromatic aberration during wave

--- CHAOS/Assets/CHAOS/ChaosTimer.cs
+++ CHAOS/Assets/CHAOS/ChaosTimer.cs
@@ -8,6 +8,8 @@ public class ChaosTimer : MonoBehaviour
     [Header("Variables")]
     [SerializeField] private float timeTilWave = 5.0f;
     [SerializeField] private bool isRandomising = false;
+    [SerializeField] private float waveTimeScale = 0.25f;
+    [SerializeField] private float timeScaleDuration = 1.5f;
 
     [Header("Refs")]
     [SerializeField] private TextMeshProUGUI textTimer = null;
@@ -17,10 +19,12 @@ public class ChaosTimer : MonoBehaviour
     private float timer = 0.0f;
 
     private PlayerInput input = null;
+    private PostProcessController postProcCtrl = null;
 
     private void Start()
     {
         input = FindObjectOfType<PlayerInput>();
+        postProcCtrl = FindObjectOfType<PostProcessController>();
     }
 
     void Update()
@@ -33,6 +37,8 @@ public class ChaosTimer : MonoBehaviour
     {
         GameObject go = Instantiate(prefWave);
         go.transform.position = wavePos.transform.position;
+
+        StartCoroutine(SlowTime());
     }
 
     public void RandomiseInputsOnTimer()
@@ -55,5 +61,38 @@ public class ChaosTimer : MonoBehaviour
     void UpdateUI()
     {
         textTimer.text = timer.ToString("f0");
+    }
+
+    IEnumerator SlowTime()
+    {
+        Time.timeScale = waveTimeScale;
+        //postProcCtrl.SetLensDistortion(1.0f);
+        //postProcCtrl.SetChromaticAb(1.0f);
+        StartCoroutine(LerpChromaticAberration(1.0f, 0.0f));
+
+        yield return new WaitForSeconds(timeScaleDuration);
+
+        //postProcCtrl.SetLensDistortion(0.0f);
+        //postProcCtrl.SetChromaticAb(0.0f);
+        StartCoroutine(LerpChromaticAberration(0.0f, 1.0f));
+        Time.timeScale = 1.0f;
+    }
+
+    IEnumerator LerpChromaticAberration(float valTo, float valFrom)
+    {
+        float elapsedTime = 0;
+        float waitTime = timeScaleDuration;
+
+        float chromVal = valFrom;
+
+        while (elapsedTime < waitTime)
+        {
+            //transform.position = Vector3.Lerp(currentPos, Gotoposition, (elapsedTime / waitTime));
+
+            chromVal = Mathf.Lerp(chromVal, valTo, (elapsedTime / waitTime));
+            postProcCtrl.SetChromaticAb(chromVal);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
---
+++ CHAOS/Assets/Camera/PostProcessController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcessController : MonoBehaviour
+{
+    private VolumeProfile prof;
+    private LensDistortion lensDist;
+    private ChromaticAberration chromAb;
+
+    private void Start()
+    {
+        prof = GetComponent<Volume>().profile;
+
+        prof.TryGet<LensDistortion>(out lensDist);
+        prof.TryGet<ChromaticAberration>(out chromAb);
+    }
+
+    public void SetLensDistortion(float val)
+    {
+        lensDist.intensity.value = val;
+    }
+
+    public void SetChromaticAb(float val)
+    {
+        chromAb.intensity.value = val;
+    }
+}
