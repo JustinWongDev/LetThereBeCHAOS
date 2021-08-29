7db9bc0ca8f38ca71ae9e2656180248a3a9e14d5
JustinWongDev <42408284+JustinWongDev@users.noreply.github.com>
2021-08-29 12:51:44 +1000

    Add BG music

---
+++ CHAOS/Assets/Utility/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private GameObject bgIntro = null;
+    [SerializeField] private GameObject bgGame = null;
+
+    //public static AudioClip Intro, Game;
+    private static AudioSource audioSrc;
+
+    private void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+
+        //Intro = UnityEngine.Resources.Load<AudioClip>("Intro");
+        //Game = UnityEngine.Resources.Load<AudioClip>("Game");
+
+        GameManager.Instance.Mainmenu.AddListener(PlayIntro);
+        GameManager.Instance.Newgame.AddListener(PlayGame);
+        GameManager.Instance.Gameover.AddListener(PlayIntro);
+    }
+
+    public void PlayIntro()
+    {
+        bgIntro.SetActive(true);
+        bgGame.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        bgIntro.SetActive(false);
+        bgGame.SetActive(true);
+    }
+
+}
