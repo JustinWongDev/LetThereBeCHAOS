e058921af2e5ad051d39dcac21a27ca6735ddbfd
JustinWongDev <42408284+JustinWongDev@users.noreply.github.com>
2021-08-29 10:32:12 +1000

    Add score system

---
+++ CHAOS/Assets/Score/OrbGen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbGen : MonoBehaviour
+{
+    [SerializeField] private Transform orbPos;
+    [SerializeField] private GameObject prefOrb = null;
+    [SerializeField] private float maxTime = 5.0f;
+    [SerializeField] private float minTime = 3.0f;
+    [SerializeField] private float xRange = 12.0f;
+    private float timer = 0.0f;
+
+    private void OnEnable()
+    {
+        timer = Random.Range(minTime, maxTime);
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            SpawnOrb();
+            timer = Random.Range(minTime, maxTime);
+        }
+    }
+
+    void SpawnOrb()
+    {
+        GameObject go = Instantiate(prefOrb, orbPos.transform);
+        Vector2 newPos = transform.position;
+        newPos.x = Random.Range(-xRange, xRange);
+        orbPos.transform.position = newPos;
+    }
+}
---
+++ CHAOS/Assets/Score/ScoreController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    [Header("Vars")]
+    [SerializeField] private int ptsPerOrb = 50;
+
+    [Header("Refs")]
+    [SerializeField] private TextMeshProUGUI textScore = null;
+
+    private PlayerController player = null;
+    private float maxYReached = 0;
+    private int numOrbsCollected = 0;
+
+    private float score;
+
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        score = maxYReached + numOrbsCollected * ptsPerOrb;
+
+        if (player.gameObject.transform.position.y > maxYReached)
+        {
+            maxYReached = player.gameObject.transform.position.y;
+        }
+
+        textScore.text = score.ToString("f0");
+    }
+
+    public void AddOrb()
+    {
+        numOrbsCollected++;
+    }
+}
